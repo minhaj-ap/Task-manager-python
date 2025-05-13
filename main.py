a879ec848eb9f9ef937b8bc5ad773c713900e951
Minhaj Ap <minhajap00@gmail.com>
2025-05-13 10:22:50 +0530

    refactor task management functions and improve user input handling

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 def addTask():
     print("Add task")
 def editTask():
@@ -7,10 +6,10 @@ def markComplete():
     print("mark complete")
 def deleteTask():
     print("Delete task")
-def getTask():
-    print("Get task")
-def getPendingTask():
-    print("Get pending task")
+def viewTask():
+    print("view task")
+def viewPendingTask():
+    print("view pending task")
 
 def main():
     print("Welcome to task manager")
@@ -20,3 +19,22 @@ def main():
     print("To delete tasks input 4")
     print("To mark complete a  task input 5")
     print("To view pending tasks input 6")
+    while True: 
+        choice=int(input("Enter your choice: "))
+        if choice==1:
+            viewTask()
+        elif choice==2:
+            editTask()
+        elif choice==3:
+            addTask()
+        elif choice==4:
+            deleteTask()
+        elif choice==5:
+            markComplete()
+        elif choice==6:
+            viewPendingTask()
+        else:
+            print("Invalid input")
+            
+if __name__ == "__main__":
+    main()
