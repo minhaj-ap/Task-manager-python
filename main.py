14ae7d72028fdffa651cf52991530c15c7d63329
Minhaj Ap <minhajap00@gmail.com>
2025-05-13 10:17:32 +0530

    add task management functions and main menu

---
+++ main.py
@@ -0,0 +1,22 @@
+
+def addTask():
+    print("Add task")
+def editTask():
+    print("Edit task")
+def markComplete():
+    print("mark complete")
+def deleteTask():
+    print("Delete task")
+def getTask():
+    print("Get task")
+def getPendingTask():
+    print("Get pending task")
+
+def main():
+    print("Welcome to task manager")
+    print("To view tasks input 1")
+    print("To edit tasks input 2")
+    print("To add tasks input 3")
+    print("To delete tasks input 4")
+    print("To mark complete a  task input 5")
+    print("To view pending tasks input 6")
